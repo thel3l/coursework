6ffaffdd22ee418f03908171fc9cb31c4cc4ac76
lunaroyster <oneinaquadrillion@live.com>
2017-12-09 12:43:14 +0000

    Cleanup

--- pracs/Version B/21.py
+++
@@ -1,7 +0,0 @@
-def Non_Diagonal(array):
-    elements = []
-    for j in range(len(array)):
-        for i in range(len(array[j])):
-            if i!=j:
-                elements.append(array[j][i])
-    return elements
